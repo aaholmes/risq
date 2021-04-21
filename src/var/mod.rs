33bb3b3c4568c4f1771cb4548b8d227865c5e6c0
Adam Holmes <adam@myralabs.com>
2021-04-20 22:37:36 -0400

    Cleanup var/mod.rs

--- src/var/mod.rs
+++ src/var/mod.rs
@@ -8,38 +8,24 @@ use crate::excite::ExciteGenerator;
 
 pub fn variational(ham: &Ham, excite_gen: &ExciteGenerator, wf: &mut Wf) {
 
+    let mut iter: i32 = 0;
+
+    println!("Start of variational stage: Wavefunction has {} determinants with energy {}", wf.n, wf.energy);
+
     while !wf.converged {
 
         wf.add_new_dets(&ham, &excite_gen);
 
         wf.optimize(&ham, &excite_gen);
 
+        iter += 1;
+
+        println!("End of iteration {}: Wavefunction has {} determinants with energy {}", iter, wf.n, wf.energy);
+
+        if wf.n <= 20 {
+            wf.print();
+        }
+
     }
 
 }
-
-
-
-
-
-//     for (iter, eps) in wf.eps_iter.enumerate() {
-//         println!("Initial wf:");
-//         wf.print();
-//
-//         println!("Starting variation {} with epsilon {}", iter, eps);
-//         // Get new dets
-//         // wf.get_new_dets(excite_gen);
-//
-//         println!("After first iteration of getting new dets:");
-//         wf.print();
-//
-//
-//
-//         // Compute variational H
-//         //var_h = compute_var_h(wf);
-//
-//         // Minimize energy using davidson
-//         //davidson(var_h, wf);
-//         //println!("End of iteration {}: Wavefunction has {} determinants with energy {}", iter, wf.n, wf.energy);
-//     }
-// }
